e4ccbcd331ad7e6e640cbe261b2a33e418f32141
Radu Mocanu <radu_mocanu2000@yahoo.com>
2021-08-04 12:54:52 +0300

    added new properties to k8s cluster

--- examples/k8s_cluster.pp
+++ examples/k8s_cluster.pp
@@ -1,7 +1,18 @@
 k8s_cluster { 'myClustertest' :
-  ensure           => present,
-  k8s_version      => '1.18.5',
-  maintenance_day  => 'Sunday',
-  maintenance_time => '14:53:00Z',
-  public           => true,
+  ensure                => present,
+  k8s_version           => '1.18.5',
+  maintenance_day       => 'Sunday',
+  maintenance_time      => '14:53:00Z',
+  public                => true,
+  api_subnet_allow_list => [
+    '1.2.3.4/32',
+    '2002::1234:abcd:ffff:c0a8:101/64',
+    '1.2.3.4',
+    '2002::1234:abcd:ffff:c0a8:101',
+  ],
+  s3_buckets            => [
+    {
+      name => 'testtest234134124214'
+    },
+  ]
 }
